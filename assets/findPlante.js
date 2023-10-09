c6041fe53049e2c8f2a98b7b2b7ac92e8a058587
Hanane Chyabri <chyabri.hanane@gmail.com>
2023-10-09 09:54:54 +0200

    script plantejs externalisé

--- assets/findPlante.js
+++ assets/findPlante.js
@@ -1,95 +1,89 @@
 import './styles/findPlante.css';
 
-// alert("ggg");
+alert("ggg");
 
 
 
-// // Axiox pour filtrer les plantes
-//     // on obtient le formulaire pour
-//     const form = document.getElementById('filtre-form-plante');
+// Axiox pour filtrer les plantes
+// on obtient le formulaire pour
+const form = document.getElementById('filtre-form-plante');
 
-//     // on detectera tous les inputs du form, peu importe quel fitre on change
-//     form.addEventListener('change', function (event) {
-//         event.preventDefault();
-//     // obtenir le div pour afficher les résultats
+// on detectera tous les inputs du form, peu importe quel fitre on change
+form.addEventListener('change', function (event) {
+    event.preventDefault();
+    // obtenir le div pour afficher les résultats
 
-//     axios({
-//         url: '{{ path("maPlante") }}',
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'multipart/form-data'
-// },
-//     data: new FormData(document.getElementById("filtre-form-plante"))
-// }).then(function (response) {
-
-
-//         let sectionPlante = document.querySelector(".fiches");
-
-//     // parcourir l'array d'objets reçu (car le JSON a été déjà parsé par AXIOS)
-//     let arrayPlantes = response.data;
-
-//     console.log(arrayPlantes);
+    axios({
+        // on prend la route generee avec path du data-route du form
+        url: form.dataset.route,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        data: new FormData(document.getElementById("filtre-form-plante"))
+    }).then(function (response) {
 
 
-//     sectionPlante.innerHTML = "";
+        let sectionPlante = document.querySelector(".fiches");
+
+        // parcourir l'array d'objets reçu (car le JSON a été déjà parsé par AXIOS)
+        let arrayPlantes = response.data;
+
+        console.log(arrayPlantes);
 
 
-//     arrayPlantes.forEach(function (plante) { // manipuler le DOM. Ex: vider un div et le remplir avec les résultats
-//         console.log(plante);
-//     const planteElement = document.createElement("article");
-
-//     // nom de la plante
-//     const nomPlante = document.createElement("P");
-//     nomPlante.innerText = plante.nom + " " + plante.exposition + "   " + plante.besoinEau + "   " + plante.lieuCultive;
-
-//     const imagePlante = document.createElement("img");
-//     // image de la plante
-//     plante.images.forEach(function (url) {
-//         imagePlante.src = "/" + url;
-// });
+        sectionPlante.innerHTML = "";
 
 
-//     sectionPlante.appendChild(planteElement);
+        arrayPlantes.forEach(function (plante) { // manipuler le DOM. Ex: vider un div et le remplir avec les résultats
+            console.log(plante);
+            const planteElement = document.createElement("article");
 
-//     planteElement.appendChild(imagePlante);
-//     planteElement.appendChild(nomPlante);
+            // nom de la plante
+            const nomPlante = document.createElement("P");
+            nomPlante.innerText = plante.nom + " " + plante.exposition + "   " + plante.besoinEau + "   " + plante.lieuCultive;
 
-
-// });
-
-
-// }).catch(error => {
-//         console.error(error);
-// });
-// });
-
-
-//     // Axiox pour ajouter au liste de souhait
+            const imagePlante = document.createElement("img");
+            // image de la plante
+            plante.images.forEach(function (url) {
+                imagePlante.src = "/" + url;
+            });
 
 
+            sectionPlante.appendChild(planteElement);
 
-//     var boutons = document.querySelectorAll('.add_wish_list');
-
-//     // Parcourez la liste des boutons et ajoutez un écouteur d'événements à chacun
-//     boutons.forEach(function(bouton) {
-//         bouton.addEventListener('click', function () {
-//             // Code à exécuter lorsque le bouton est cliqué
-//             console.log("Bouton cliqué !");
-
-//             event.preventDefault();
-//             const planteId = event.target.getAttribute('data-id');
+            planteElement.appendChild(imagePlante);
+            planteElement.appendChild(nomPlante);
 
 
+        });
 
 
-
-//             // Envoyez une requête Axios pour ajouter la plante à la liste de souhaits
-//             axios.post(`/plante/listSouhait/${planteId}`).then(function (response) { // Gérez la réponse du serveur (par exemple, actualisez l'interface utilisateur)
-//                 console.log(response);
-//             }).catch(function (error) {
-//                 console.error('Une erreur s\'est produite lors de l\'ajout à la liste de souhaits', error);
-//             });
+    }).catch(error => {
+        console.error(error);
+    });
+});
 
 
-//         });
-// });
+// Axiox pour ajouter au liste de souhait
+var boutons = document.querySelectorAll('.add_wish_list');
+
+// Parcourez la liste des boutons et ajoutez un écouteur d'événements à chacun
+boutons.forEach(function (bouton) {
+    bouton.addEventListener('click', function () {
+        // Code à exécuter lorsque le bouton est cliqué
+        console.log("Bouton cliqué !");
+
+        event.preventDefault();
+        const planteId = event.target.getAttribute('data-id');
+
+        // Envoyez une requête Axios pour ajouter la plante à la liste de souhaits
+        axios.post(`/plante/listSouhait/${planteId}`).then(function (response) { // Gérez la réponse du serveur (par exemple, actualisez l'interface utilisateur)
+            console.log(response);
+        }).catch(function (error) {
+            console.error('Une erreur s\'est produite lors de l\'ajout à la liste de souhaits', error);
+        });
+
+
+    });
+});
