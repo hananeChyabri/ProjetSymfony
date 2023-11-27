03ef29d49e858ec840ed756fa346dcbe460d05d8
Hanane Chyabri <chyabri.hanane@gmail.com>
2023-11-27 13:42:35 +0100

    modif css

--- migrations/Version20231127121918.php
+++ migrations/Version20231127121918.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231116134811 extends AbstractMigration
+final class Version20231127121918 extends AbstractMigration
 {
     public function getDescription(): string
     {
